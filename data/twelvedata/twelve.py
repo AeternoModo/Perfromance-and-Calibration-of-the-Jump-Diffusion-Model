a1294cd3678dfcc36378b01bf23e3be477c303c9
AeternoModo <105218336+AeternoModo@users.noreply.github.com>
2026-08-28 09:00:22 +0000

    Setting up the api as a class. Extracting the data for a set of chosen stocks in certain time settings. Preparing the data for backtesting library application.

--- data/twelvedata/twelve.py
+++ data/twelvedata/twelve.py
@@ -5,30 +5,39 @@ from pprint import pprint
 import requests 
 import pandas as pd
 
-#API access
+#API access and url grab
 api_key = config('TWELVEDATA_KEY')
+base_url = 'https://api.twelvedata.com/time_series'
 
-#test-params
-symbol = 'AAPL'
-interval = '1day'
-start_date = '2023-01-01'
-end_date = '2023-01-31'
+#Ticker-Class: Insert Variables for your stock of interest and output basic information as a pandas file
+class StockTicker:
+    def __init__(self,symbol,interval,start_date,end_date):
+        self.symbol = symbol
+        self.interval = interval
+        self.start_date = start_date
+        self.end_date = end_date
 
-base_erl = 'https://api.twelvedata.com/time_series'
+    def get_data(self,show=False):
+        #parameters for the request
+        params = {
+            'symbol': self.symbol,
+            'interval': self.interval,
+            'start_date': self.start_date,
+            'end_date': self.end_date,
+            'apikey': api_key,
+            #'outputsize': 5000
+        }
 
-params = {
-    'symbol': symbol,
-    'interval': interval,
-    'start_date': start_date,
-    'end_date': end_date,
-    'apikey': api_key
-}
+        #request to the API
+        response = requests.get(base_url, params=params)
+        data = pd.DataFrame(response.json()['values'])
+        if show:
+            print(data)
+        return data
 
-#get request
-response = requests.get(base_erl, params=params)
+#Example of use
+#stock1 = StockTicker(symbol='AAPL', interval='1day', start_date='2023-01-01', end_date='2023-01-31')
+#data = stock1.get_data(show=True)
 
-pprint(response.json())
 
-#convert to dataframe
-data = pd.DataFrame(response.json()['values'])
-print(data)
+
